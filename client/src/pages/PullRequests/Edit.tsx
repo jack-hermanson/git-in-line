975cd4e8397ad1c8379b1af4cd50622b6e5e03a9
Jack Hermanson <jack.hermanson@live.com>
2021-06-11 12:18:48 -0600

    find matching pr

--- client/src/pages/PullRequests/Edit.tsx
+++ client/src/pages/PullRequests/Edit.tsx
@@ -1,25 +1,41 @@
-import React from "react";
+import React, {useEffect} from "react";
 import {useHistory} from "react-router-dom";
 import {PageTitle} from "../../components/Layout/PageTitle";
 import {Col, Row} from "reactstrap";
 import {RouteComponentProps} from "react-router";
+import {useStoreState} from "../../store";
+import {LoadingSpinner} from "../../components/Utils/LoadingSpinner";
 
-export const Edit: React.FC<RouteComponentProps<{id: string}>> = ({match}) => {
+export const Edit: React.FC<RouteComponentProps<{ id: string }>> = ({match}) => {
 
     const history = useHistory();
+    const currentUser = useStoreState(state => state.currentUser);
+    const pullRequest = useStoreState(state => state.pullRequests)
+        ?.find(pr => pr.id === parseInt(match.params.id));
+
+    useEffect(() => {
+        if (!currentUser) {
+            history.push("/account/login");
+        }
+    }, [history, currentUser]);
 
     return (
         <React.Fragment>
             <Row>
                 <Col>
-                    <PageTitle text="Edit Pull Request" />
+                    <PageTitle text="Edit Pull Request"/>
                 </Col>
             </Row>
-            <Row>
-                <Col>
-                    <p>Editing PR with ID {match.params.id}</p>
-                </Col>
-            </Row>
+            {pullRequest ? (
+                <React.Fragment>
+                    <Row>
+                        <Col>
+                            <p>Editing PR with URL {pullRequest.gitHubUrl}</p>
+                        </Col>
+                    </Row>
+                </React.Fragment>
+            ) : <LoadingSpinner />}
+
         </React.Fragment>
     );
 }
