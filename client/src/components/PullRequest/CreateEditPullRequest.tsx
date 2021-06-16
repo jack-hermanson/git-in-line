8fe88277279b947c65ab3c1900337e0d2716f04a
Jack Hermanson <jack.hermanson@live.com>
2021-06-15 23:44:41 -0600

    loop through priority enum on edit

--- client/src/components/PullRequest/CreateEditPullRequest.tsx
+++ client/src/components/PullRequest/CreateEditPullRequest.tsx
@@ -1,7 +1,7 @@
 import React, {useState} from "react";
 import {Button, FormGroup, Input, Label} from "reactstrap";
 import {NewPrRequest, PullRequestRecord} from "../../../../shared/src/resource_models/pullRequest";
-import {Priority} from "../../../../shared/src/enums";
+import {Priority, PriorityEnumLabel, PrStatusEnumLabel} from "../../../../shared/src/enums";
 
 interface Props {
     onSubmit: (newPr: NewPrRequest) => any;
@@ -9,6 +9,9 @@ interface Props {
 }
 
 export const CreateEditPullRequest: React.FC<Props> = ({onSubmit, existingPr}: Props) => {
+
+    console.log("hello");
+    console.log(Array.from(PrStatusEnumLabel));
 
     const [gitHubUrl, setGitHubUrl] = useState(existingPr?.gitHubUrl || "");
     const [priority, setPriority] = useState(existingPr?.priority || Priority.MED);
@@ -67,9 +70,9 @@ export const CreateEditPullRequest: React.FC<Props> = ({onSubmit, existingPr}: P
             <FormGroup>
                 <Label for={id} className="form-label required">Priority</Label>
                 <Input type="select" value={priority} onChange={e => setPriority(parseInt(e.target.value))}>
-                    <option value={Priority.LOW}>Low</option>
-                    <option value={Priority.MED}>Medium</option>
-                    <option value={Priority.HIGH}>High</option>
+                    {Array.from(PrStatusEnumLabel).map(prStatus => (
+                        <option key={prStatus[0]} value={prStatus[0]}>{prStatus[1]}</option>
+                    ))}
                 </Input>
             </FormGroup>
         );
--- shared/src/enums.ts
+++ shared/src/enums.ts
@@ -16,11 +16,23 @@ export enum PrStatus {
     APPROVED = 3
 }
 
+export const PrStatusEnumLabel = new Map<PrStatus, string>([
+    [PrStatus.PENDING, "Pending"],
+    [PrStatus.CHANGES, "Changes Requested"],
+    [PrStatus.APPROVED, "Approved"]
+]);
+
 export enum Priority {
     HIGH = 1,
     MED = 2,
     LOW = 3
 }
+
+export const PriorityEnumLabel = new Map<Priority, string>([
+    [Priority.LOW, "Low"],
+    [Priority.MED, "Medium"],
+    [Priority.HIGH, "High"]
+]);
 
 export enum SocketEvent {
     MODIFY_PULL_REQUESTS = "modify_pull_requests"
