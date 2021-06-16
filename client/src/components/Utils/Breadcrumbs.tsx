448434af5d57f5dc1a91232fe3d16961e5baf3b4
Jack Hermanson <jack.hermanson@live.com>
2021-06-16 10:21:42 -0600

    add breadcrumbs

---
+++ client/src/components/Utils/Breadcrumbs.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { Breadcrumb, BreadcrumbItem } from "reactstrap";
+import { Link } from "react-router-dom";
+
+interface Props {
+    links: { path: string; label: string }[];
+}
+
+export const Breadcrumbs: React.FC<Props> = ({ links }: Props) => {
+    return (
+        <Breadcrumb>
+            {links.map((link, index) => (
+                <BreadcrumbItem active={index === links.length - 1}>
+                    <Link to={link.path}>{link.label}</Link>
+                </BreadcrumbItem>
+            ))}
+        </Breadcrumb>
+    );
+};
